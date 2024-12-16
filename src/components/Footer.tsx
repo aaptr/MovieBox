1e5af7b58ea5b29f1a13ddda58176675e5123aa9
Oleksii Petrychenko <aa.petrichenko@gmail.com>
2024-12-16 23:17:27 +0100

    footer refactor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 export function Footer() {
   return (
-    <footer className="pt-5 mt-auto">
-      <div className="d-flex py-4 border-top border-secondary">
-        <span className="text-body-secondary">©2024 MovieBox</span>
-        <span className="text-body-secondary ms-auto">All rights reserved</span>
+    <footer className="pt-1 px-8 mt-auto w-full
+    bg-gray-800 text-white dark:border-t dark:border-gray-700">
+      <div className="flex justify-between py-4 text-lg font-semibold">
+        <span className="">©2024 MovieBox</span>
+        <span className="">All rights reserved</span>
       </div>
     </footer>
   )
