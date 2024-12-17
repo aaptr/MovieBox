96237ad781e22ddd9071531d92ee3cd5b4d28013
Oleksii Petrychenko <aa.petrichenko@gmail.com>
2024-12-17 20:22:41 +0100

    feat: Logo added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,19 @@ import { UserAvatar } from '@/components/UserAvatar'
 import { LanguageSwitcher } from '@/components/LanguageSwitcher'
 import { QuickSearch } from '@/components/QuickSearch'
 import { MenuBar } from '@/components/MenuBar'
+import logo from '@/assets/logo.webp'
 
 export function Header() {
   return (
     <header className="fixed top-0 w-full p-3 z-40 flex justify-between items-center
     bg-gray-800 text-white dark:border-b dark:border-gray-700">
-      <div className="flex gap-10 items-center">
-        <NavLink to="/">
-          <h1 className="text-3xl font-bold text-indigo-300">MovieBox</h1>
-        </NavLink>
+      <div className="flex gap-12 items-center">
+        <div className="ps-2 flex gap-3 items-center">
+          <img src={logo} alt="" className="w-12" />
+          <NavLink to="/">
+            <h1 className="text-3xl font-bold text-indigo-300">MovieBox</h1>
+          </NavLink>
+        </div>
         <MenuBar />
       </div>
       <div className="flex gap-10 items-center">
